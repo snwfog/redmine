b675b81136b95effd5992ad938d7014c5fe36451
Charles Yang <cchaoyang@8d.com>
2013-07-23 16:55:56 -0400

    Display custom project field in project list views

---
+++ lib/redmine_treeky/patches/custom_field_patch.rb
@@ -0,0 +1,23 @@
+require_dependency 'custom_field'
+
+module RedmineTreeky
+  module Patches
+    module CustomFieldPatch
+      def self.included(base) # :nodoc:
+        base.extend(ClassMethods)
+        # base.send(:include, InstanceMethods)
+
+      end
+
+      module ClassMethods
+        def custom_field(field)
+          CustomField.where("type = ?", field);
+        end
+      end
+    end
+  end
+end
+
+unless CustomField.included_modules.include?(RedmineTreeky::Patches::CustomFieldPatch)
+  CustomField.send(:include, RedmineTreeky::Patches::CustomFieldPatch)
+end
