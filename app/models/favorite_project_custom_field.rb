13f04ca93cda59810b1da23fa7df1cc5976996c2
Charles Yang <cchaoyang@8d.com>
2013-07-25 15:56:08 -0400

    Fix the display issue in view as well as testing persistance

--- app/models/favorite_project_custom_field.rb
+++ app/models/favorite_project_custom_field.rb
@@ -1,9 +1,9 @@
 class FavoriteProjectCustomField < ActiveRecord::Base
   unloadable
 
-  validates :custom_field_id, inclusion: { in: CustomField.project.collect(&:id) }
-  validates :custom_field_id, presence: :true
-  validates :user_id, presence: :true
+  # validates :custom_field_id, inclusion: { in: CustomField.project.collect(&:id) }
+  # validates :custom_field_id, presence: :true
+  # validates :user_id, presence: :true
 
   def self.fav(user_id, custom_field_id)
     self.find_by_user_id_and_custom_field_id(user_id, custom_field_id)
