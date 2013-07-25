d561f9e739b52b74b3b605b16e2880ae5033b906
Charles Yang <cchaoyang@8d.com>
2013-07-25 14:20:42 -0400

    Persistance with project favorite to show, albeit display is still buggy

--- plugins/redmine_treeky/app/controllers/project_custom_label_filter_controller.rb
+++ plugins/redmine_treeky/app/controllers/project_custom_label_filter_controller.rb
@@ -3,6 +3,16 @@ class ProjectCustomLabelFilterController < ApplicationController
   before_filter :get_user
 
   def update
+    field_ids = params[:project_custom_field]
+    field_ids.each do |field, check|
+      favorite = FavoriteProjectCustomField.fav(@user.id, field)
+      if check == "1" && favorite.nil?
+        FavoriteProjectCustomField.create(user_id: @user.id, custom_field_id: field)
+      elsif check == "0" && favorite
+        FavoriteProjectCustomField.delete_all(user_id: @user.id, custom_field_id: field)
+      end
+    end
+
     respond_to do |format|
       format.html { redirect_to :back }
       format.js { render partial: 'set_filter' }
